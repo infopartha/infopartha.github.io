de00eae726e5bf0e82ddd4ef0a9dd3566105ecac
infopartha <parthasarathy.rps@gmail.com>
2024-06-11 23:49:30 -0400

    Added visit counter and minor changes

--- scripts.js
+++ scripts.js
@@ -77,3 +77,13 @@ for (i = 0; i < coll.length; i++) {
     }
   });
 }
+
+// Smooth scrolling
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+      e.preventDefault();
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+          behavior: 'smooth'
+      });
+  });
+});
